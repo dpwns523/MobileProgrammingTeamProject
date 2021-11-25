027d44772cca1f255da00153619bd3a18ed17e14
dpwns523 <84260096+dpwns523@users.noreply.github.com>
2021-11-25 12:43:35 +0900

    Add files via upload

---
+++ ProPt/TeamProject_ProPt/app/src/main/java/koreatech/teamproject_propt/MainActivity.java
@@ -0,0 +1,20 @@
+package koreatech.teamproject_propt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/*
+    한국기술교육대학교 컴퓨터공학부
+    2021-2학기 모바일프로그래밍 팀프로젝트
+    팀원 : 임예준, 남태민, 이원진, 이동훈
+
+    MainActivity 로그인 화면 구성.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
