a7d44c4d671217cf9823beab1edccdc9d748ccc1
woosdf <kacaw00d@naver.com>
2021-12-11 20:06:46 +0900

    운동방법 UI 추가

---
+++ TeamProject_ProPt/app/src/main/java/koreatech/teamproject_propt/main_exercise_way.java
@@ -0,0 +1,14 @@
+package koreatech.teamproject_propt;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class main_exercise_way extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_exercise_way);
+    }
+}
