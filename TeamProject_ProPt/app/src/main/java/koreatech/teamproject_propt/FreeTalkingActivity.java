5544e99e9b986ba8a45356ec353a20ea8faf034a
ntm309 <ntm309@gmail.com>
2021-12-14 14:31:04 +0900

    남태민 운동UI추가

---
+++ TeamProject_ProPt/app/src/main/java/koreatech/teamproject_propt/FreeTalkingActivity.java
@@ -0,0 +1,14 @@
+package koreatech.teamproject_propt;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FreeTalkingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.free_talking_main);
+    }
+}
