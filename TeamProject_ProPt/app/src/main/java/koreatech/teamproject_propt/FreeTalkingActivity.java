45eb5a8c356839e02220a3119262ac9c86f6f372
ntm309 <ntm309@gmail.com>
2021-12-15 13:25:23 +0900

    layout 이름 변경

--- TeamProject_ProPt/app/src/main/java/koreatech/teamproject_propt/FreeTalkingActivity.java
+++ TeamProject_ProPt/app/src/main/java/koreatech/teamproject_propt/FreeTalkingActivity.java
@@ -9,6 +9,6 @@ public class FreeTalkingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.free_talking_main);
+        setContentView(R.layout.activity_freetalking);
     }
 }
