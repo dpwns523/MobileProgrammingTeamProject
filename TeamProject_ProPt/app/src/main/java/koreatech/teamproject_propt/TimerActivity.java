15f91d41f419063f22bd772a9bc9c46584fc871d
YeJunLim <dpwns523@gmail.com>
2021-12-16 15:11:36 +0900

    Timer Activity 구현

--- TeamProject_ProPt/app/src/main/java/koreatech/teamproject_propt/TimerActivity.java
+++ TeamProject_ProPt/app/src/main/java/koreatech/teamproject_propt/TimerActivity.java
@@ -20,6 +20,14 @@ import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.concurrent.TimeUnit;
+/*
+    타이머 액티비티
+    Interver, 30초, 1분, 직접 입력 기능 구현
+    Interver은 1분 30초로 고정되어있으며 1분 30초 동안 쉬지않고 빠르게 운동하는 방법을 의미
+    30sec와 1min버튼은 30초와 1분을 설정하는 기능으로 30초, 1초씩 타이머를 추가하는 기능이 아님.
+    보통 운동은 한 세트에 30초와 1분내로 진행되기 때문에 간편하게 이용하기 위함.
+    직접 입력가능 ex) 1분 30초 -> 1.5
+ */
 
 public class TimerActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -60,7 +68,6 @@ public class TimerActivity extends AppCompatActivity implements View.OnClickList
 
 
     }
-
     /**
      *  뷰 초기화
      */
@@ -89,21 +96,36 @@ public class TimerActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.imageViewReset:
+            case R.id.imageViewReset:   // 타이머 실행 시 시간초 리셋 버튼
                 reset();
                 break;
-            case R.id.imageViewStartStop:
+            case R.id.imageViewStartStop:   // 맨 아래 시작버튼 -> 타이머 진행 시 스탑버튼으로 변경됨
+                // 직접입력란에 원하는 시간을 넣으면 우선순위는 직접입력한 시간이 됨.
+                double time = 0.0;
+                if (!editTextMinute.getText().toString().isEmpty()) {
+                    time = Double.parseDouble(editTextMinute.getText().toString().trim());
+                    timeCountInMilliSeconds = (long) (time * 60 * 1000);
+                    setProgressBarValues();
+                }
                 startStop();
                 break;
-            case R.id.button:
+            case R.id.button:   // Interval -> 인터벌트레이닝 -> 빠르게 운동하고 휴식하는 운동방법(유산소 운동의 일종) 무조건 1분 30초
+                setTimerValues(1.5);
+                setProgressBarValues();
+
                 break;
-            case R.id.button2:
+            case R.id.button2:  // 30SEC
+                setTimerValues(0.5);
+                setProgressBarValues();
+
                 break;
-            case R.id.button3:
+            case R.id.button3:  // 1Min
+                setTimerValues(1);
+                setProgressBarValues();
+
                 break;
         }
     }
-
     /**
      * 카운트 다운 시간을 리셋하고 재시작하는 기능
      */
@@ -118,11 +140,6 @@ public class TimerActivity extends AppCompatActivity implements View.OnClickList
     private void startStop() {
 
         if (timerStatus == TimerStatus.STOPPED) {
-
-
-            setTimerValues();
-            setProgressBarValues();
-
             imageViewReset.setVisibility(View.VISIBLE);
             imageViewStartStop.setImageResource(R.drawable.ic_baseline_stop_circle_24);
             editTextMinute.setEnabled(false);
@@ -144,30 +161,19 @@ public class TimerActivity extends AppCompatActivity implements View.OnClickList
         }
 
     }
-
     /**
      * 타이머에 시간이 설정 되어있는지 체크하크
      *  - 있는 경우 : 타이머에 시간 세팅
      *  - 없는 경우 : 시간을 설정해달라는 안내 토스트 메세지
      */
-    private void setTimerValues() {
-        int time = 0;
-        if (!editTextMinute.getText().toString().isEmpty()) {
-
-            time = Integer.parseInt(editTextMinute.getText().toString().trim());
-        } else {
-
-            Toast.makeText(getApplicationContext(), "시간을 설정해주세요", Toast.LENGTH_LONG).show();
-        }
-
-        timeCountInMilliSeconds = time * 60 * 1000;
+    private void setTimerValues(double time) {
+        timeCountInMilliSeconds = (long) (time * 60 * 1000);
     }
 
     /**
      * 카운트다운 시작 기능
      */
     private void startCountDownTimer() {
-
         countDownTimer = new CountDownTimer(timeCountInMilliSeconds, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -209,9 +215,9 @@ public class TimerActivity extends AppCompatActivity implements View.OnClickList
      * 원형 프로그레스 바에 값 세팅
      */
     private void setProgressBarValues() {
-
         progressBarCircle.setMax((int) timeCountInMilliSeconds / 1000);
         progressBarCircle.setProgress((int) timeCountInMilliSeconds / 1000);
+        textViewTime.setText(hmsTimeFormatter((long) timeCountInMilliSeconds));
     }
 
 
