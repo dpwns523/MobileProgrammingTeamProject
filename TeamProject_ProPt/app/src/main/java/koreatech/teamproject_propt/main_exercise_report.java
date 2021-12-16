38455f24b86f2bd47f087544d726b761929e74eb
ntm309 <ntm309@gmail.com>
2021-12-16 22:41:33 +0900

    graph, spec, workout record

--- TeamProject_ProPt/app/src/main/java/koreatech/teamproject_propt/main_exercise_report.java
+++ TeamProject_ProPt/app/src/main/java/koreatech/teamproject_propt/main_exercise_report.java
@@ -1,13 +1,27 @@
 package koreatech.teamproject_propt;
 
+import android.app.LauncherActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
 
 /*
     일일 운동 목표 설정 Activity
@@ -17,6 +31,12 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class main_exercise_report extends AppCompatActivity {
     private Context mContext = this;
     private static final int ACTIVITY_NUM = 4;
+    private ListAdapter adapter;
+    private ListView listView;
+    private int num;
+
+    private ListView m_ListView;
+    private ArrayAdapter<String> m_Adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
