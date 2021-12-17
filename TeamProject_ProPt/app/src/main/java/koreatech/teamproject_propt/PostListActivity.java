c5f8efc739490d61b8a71d8937a10e55247a493b
desfox <desfox99@naver.com>
2021-12-18 03:09:26 +0900

    firebase 커뮤니티 구현

--- TeamProject_ProPt/app/src/main/java/koreatech/teamproject_propt/PostListActivity.java
+++ TeamProject_ProPt/app/src/main/java/koreatech/teamproject_propt/PostListActivity.java
@@ -2,18 +2,13 @@ package koreatech.teamproject_propt;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -28,9 +23,9 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /*
     게시글 목록 출력하는 액티비티
@@ -49,80 +44,76 @@ public class PostListActivity extends AppCompatActivity implements PostRVAdapter
     private PostRVAdapter postRVAdapter;
     private RelativeLayout homeRL;
 
-    // floating action 버튼, progressBar 사용
+    // floating action 버튼 사용
     private FloatingActionButton addPostFAB;
-    private ProgressBar PBloadingRV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_list);
-        // initializing all our variables.
+
         postRV = findViewById(R.id.postRV);
         homeRL = findViewById(R.id.bottomSheetRL);
-        PBloadingRV = findViewById(R.id.PBLoadingRV);
         addPostFAB = findViewById(R.id.addPostFAB);
         firebaseDatabase = FirebaseDatabase.getInstance();
         mAuth = FirebaseAuth.getInstance();
+
         postRVModalArrayList = new ArrayList<>();
-        // on below line we are getting database reference.
+
+        // 데이터베이스 객체 가져옴
         databaseReference = firebaseDatabase.getReference("Posts");
-        // on below line adding a click listener for our floating action button.
+
         addPostFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // opening a new activity for adding a course.
+                // 버튼 클릭 시 게시글 작성
                 Intent intent = new Intent(PostListActivity.this, AddPostActivity.class);
                 startActivity(intent);
             }
         });
-        // on below line initializing our adapter class.
+
+        // 어댑터 생성
         postRVAdapter = new PostRVAdapter(postRVModalArrayList, this, this::onPostClick);
-        // setting layout malinger to recycler view on below line.
+
+        // recycle view를 위한 layout 설정
         postRV.setLayoutManager(new LinearLayoutManager(this));
-        // setting adapter to recycler view on below line.
+
+        // 어댑터 등록
         postRV.setAdapter(postRVAdapter);
-        // on below line calling a method to fetch courses from database.
+
+        // 데이터베이스에서 게시글 불러옴
         getPosts();
     }
 
     private void getPosts() {
-        // on below line clearing our list.
         postRVModalArrayList.clear();
-        // on below line we are calling add child event listener method to read the data.
+
+        // 데이터베이스의 child에 이벤트 리스너 등록
         databaseReference.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                // on below line we are hiding our progress bar.
-//                PBloadingRV.setVisibility(View.GONE);
-                // adding snapshot to our array list on below line.
+                // array list에 스냅샷 더함
                 postRVModalArrayList.add(snapshot.getValue(PostRVModal.class));
-                // notifying our adapter that data has changed.
+                // 어댑터에 데이터가 갱신되었음을 알림
                 postRVAdapter.notifyDataSetChanged();
             }
 
             @Override
             public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                // this method is called when new child is added
-                // we are notifying our adapter and making progress bar
-                // visibility as gone.
-                PBloadingRV.setVisibility(View.GONE);
+                // child 등록 시 불림
                 postRVAdapter.notifyDataSetChanged();
             }
 
             @Override
             public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-                // notifying our adapter when child is removed.
+                // 어댑터에 데이터가 삭제되었음을 알림
                 postRVAdapter.notifyDataSetChanged();
-                PBloadingRV.setVisibility(View.GONE);
-
             }
 
             @Override
             public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                // notifying our adapter when child is moved.
+                // 어댑터에 데이터가 이동되었음을 알림
                 postRVAdapter.notifyDataSetChanged();
-                PBloadingRV.setVisibility(View.GONE);
             }
 
             @Override
@@ -132,94 +123,66 @@ public class PostListActivity extends AppCompatActivity implements PostRVAdapter
         });
     }
 
+    // 게시글 클릭 시 화면 하단에서 창이 올라와 상세 정보 출력
     @Override
     public void onPostClick(int position) {
-        // calling a method to display a bottom sheet on below line.
         displayBottomSheet(postRVModalArrayList.get(position));
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-//        // adding a click listener for option selected on below line.
-//        int id = item.getItemId();
-//        switch (id) {
-//            case R.id.idLogOut:
-//                // displaying a toast message on user logged out inside on click.
-//                Toast.makeText(getApplicationContext(), "User Logged Out", Toast.LENGTH_LONG).show();
-//                // on below line we are signing out our user.
-//                mAuth.signOut();
-//                // on below line we are opening our login activity.
-//                Intent i = new Intent(MainActivity.this, LoginActivity.class);
-//                startActivity(i);
-//                this.finish();
-//                return true;
-//            default:
-//                return super.onOptionsItemSelected(item);
-//        }
-//    }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // on below line we are inflating our menu
-//        // file for displaying our menu options.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-
     private void displayBottomSheet(PostRVModal modal) {
-        // on below line we are creating our bottom sheet dialog.
+        // BottomSheetDialog 생성
         final BottomSheetDialog bottomSheetTeachersDialog = new BottomSheetDialog(this, R.style.BottomSheetDialogTheme);
-        // on below line we are inflating our layout file for our bottom sheet.
+
+        // bottom sheet에 layout inflating
         View layout = LayoutInflater.from(this).inflate(R.layout.bottom_sheet_layout, homeRL);
-        // setting content view for bottom sheet on below line.
         bottomSheetTeachersDialog.setContentView(layout);
-        // on below line we are setting a cancelable
+
+        // bottom sheet 빠져나가는 방법 설정
         bottomSheetTeachersDialog.setCancelable(false);
         bottomSheetTeachersDialog.setCanceledOnTouchOutside(true);
-        // calling a method to display our bottom sheet.
+
         bottomSheetTeachersDialog.show();
-        // on below line we are creating variables for
-        // our text view and image view inside bottom sheet
-        // and initialing them with their ids.
+
+        // bottom sheet에 출력할 정보
         TextView postNameTV = layout.findViewById(R.id.bottomSheetPostName);
         TextView postDescTV = layout.findViewById(R.id.bottomSheetPostDesc);
-
-//        TextView suitedForTV = layout.findViewById(R.id.idTVSuitedFor);
-
         TextView postCategoryTV = layout.findViewById(R.id.bottomSheetPostCategory);
         ImageView postImgIV = layout.findViewById(R.id.bottomSheetPostImg);
-        // on below line we are setting data to different views on below line.
+
+        // 정보 설정
         postNameTV.setText(modal.getPostName());
         postDescTV.setText(modal.getPostDesc());
-
-//        suitedForTV.setText("Suited for " + modal.getBestSuitedFor());
-
         postCategoryTV.setText(modal.getPostCategory());
-        Picasso.get().load(modal.getPostImgLink()).into(postImgIV);
-        Button viewBtn = layout.findViewById(R.id.editPostBtn);
-        Button editBtn = layout.findViewById(R.id.detailPostBtn);
 
-        // adding on click listener for our edit button.
+        // 카테고리에 따라 이미지 변경
+        switch(modal.getPostCategory()) {
+            case "자유":
+                postImgIV.setImageResource(R.drawable.goal);
+                break;
+            case "질문":
+                postImgIV.setImageResource(R.drawable.ic_baseline_emoji_people_24);
+                break;
+            case "운동 팁":
+                postImgIV.setImageResource(R.drawable.ic_baseline_sports_kabaddi_24);
+                break;
+            case "식단 공유":
+                postImgIV.setImageResource(R.drawable.ic_baseline_no_food_24);
+                break;
+        }
+
+        Button editBtn = layout.findViewById(R.id.editPostBtn);
+
+        // 게시글 수정 버튼에 이벤트 리스너 등록
         editBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // on below line we are opening our EditCourseActivity on below line.
                 Intent intent = new Intent(PostListActivity.this, EditPostActivity.class);
-                // on below line we are passing our course modal
+
+                // 인탠트로 게시글에 대한 모달 전달
                 intent.putExtra("post", modal);
                 startActivity(intent);
             }
         });
-        // adding click listener for our view button on below line.
-//        viewBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // on below line we are navigating to browser
-//                // for displaying course details from its url
-//                Intent intent = new Intent(PostListActivity.this, EditPostActivity.class);
-//                intent.putExtra("post", modal);
-//                startActivity(intent);
-//            }
-//        });
+
     }
 }
